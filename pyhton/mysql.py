36d7b56cb70e1aafb21cea076779ffc51e1ee765
jaehlee <jaeh.lee@navercorp.com>
2020-10-28 14:26:26 +0900

    python mysql 스크립트 예시

---
+++ pyhton/mysql.py
@@ -0,0 +1,22 @@
+import time
+import pymysql
+import datetime
+
+
+def delete_raw_stat(delete_count):
+    conn = pymysql.connect(host='localhost', port=3306, user='root', password='1234', db='test', charset='utf8')
+    curs = conn.cursor()
+    sql = "SELECT FROM USER WHERE `time` < '2020-08-28' LIMIT %s"
+
+    while True:
+        deleted_row_count = curs.execute(sql, delete_count)
+        print("[{}] Deleted row count: {}".format(datetime.datetime.now(), delete_count))
+        if deleted_row_count < 1:
+            break
+        time.sleep(1)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    delete_raw_stat(100000)
